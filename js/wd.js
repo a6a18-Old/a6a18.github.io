0bab544d5847994a87e8446e7db5ccec50bfea6b
a6a18 <a6a018@yahoo.com.tw>
2020-10-24 00:42:20 +0800

    picture

--- js/wd.js
+++ js/wd.js
@@ -19,8 +19,8 @@ var browser={
     language:(navigator.browserLanguage || navigator.language).toLowerCase()
     }
 
-var mobile_banner = '<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel"><div class="carousel-inner"><div class="carousel-item active"><img src="https://a6a18.github.io/img/cat.jpg" class="d-block w-100" alt="..."></div></div></div>';
-var desktop_banner = '<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel"><div class="carousel-inner"><div class="carousel-item active"><img src="http://homegoodies.com.tw/wp-content/uploads/2020/10/0b-cat-1-scaled-e1602429209120.jpg" class="d-block w-100" alt="..."></div></div></div>';
+var mobile_banner = '<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel"><div class="carousel-inner"><div class="carousel-item active"><img src="https://a6a18.github.io/img/mobile_cat.jpg" class="d-block w-100" alt="..."></div></div></div>';
+var desktop_banner = '<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel"><div class="carousel-inner"><div class="carousel-item active"><img src="https://a6a18.github.io/img/desktop_cat.jpg" class="d-block w-100" alt="..."></div></div></div>';
 
 var banner = desktop_banner;
 
